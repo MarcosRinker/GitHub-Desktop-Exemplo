868a188e2e8662d95a94a99616f79bb5b4e4aba4
MarcosRinker <106207184+MarcosRinker@users.noreply.github.com>
2023-06-26 15:40:01 -0300

    Revert "Hello World"
    This reverts commit 491865e731248a46b6a871fb048f57ad93a14974.

--- GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
+++ GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
@@ -14,7 +14,7 @@ public class GitHubDesktopExemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // TODO code application logic here
     }
     
 }
